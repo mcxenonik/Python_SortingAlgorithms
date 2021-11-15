fc45dbbcf89ee594241a6a362c10bc001ffc9f7a
pwisznie <p.wiszniewski@stud.elka.pw.edu.pl>
2021-11-15 14:07:33 +0100

    optimize bubble sort

--- BubbleSort.py
+++ BubbleSort.py
@@ -9,11 +9,13 @@ class BubbleSort():
     def run_algorithm(table):
         table_len = len(table)
         for i in range(table_len):
+            is_swap = False
             for j in range(0, table_len - i - 1):
                 if table[j] > table[j + 1]:
-                    temp = table[j]
-                    table[j] = table[j + 1]
-                    table[j + 1] = temp
+                    table[j], table[j+1] = table[j+1], table[i]
+                    is_swap = True
+            if not is_swap:
+                break
 
         # return table
         return table
